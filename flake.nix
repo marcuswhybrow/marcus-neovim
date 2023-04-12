89ad482124ddb018461324a2d89c95afb330049c
Marcus Whybrow <marcus@whybrow.uk>
2023-04-12 07:38:13 +0100

    Add treesitter

--- flake.nix
+++ flake.nix
@@ -7,32 +7,64 @@
   outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system: let
     pkgs = nixpkgs.legacyPackages.${system};
 
-    neovim = pkgs.wrapNeovimUnstable pkgs.neovim-unwrapped (pkgs.neovimUtils.makeNeovimConfig {
-      # https://github.com/NixOS/nixpkgs/blob/db24d86dd8a4769c50d6b7295e81aa280cd93f35/pkgs/applications/editors/neovim/utils.nix#L24
-      withPython3 = false; # defaults to true
-      extraPython3Packages = _: [ ];
-      withNodeJs = false;
-      withRuby = false; # defaults to true
-      extraLuaPackages = _: [ ];
-      plugins = [];
-      customRC = '''';
+    marcusNeovim = { stdenv, makeBinaryWrapper }: let
+      neovim = pkgs.wrapNeovimUnstable pkgs.neovim-unwrapped (pkgs.neovimUtils.makeNeovimConfig {
+        # https://github.com/NixOS/nixpkgs/blob/db24d86dd8a4769c50d6b7295e81aa280cd93f35/pkgs/applications/editors/neovim/utils.nix#L24
+        withPython3 = false; # defaults to true
+        extraPython3Packages = _: [ ];
+        withNodeJs = false;
+        withRuby = false; # defaults to true
+        extraLuaPackages = _: [ ];
+        customRC = '''';
 
-      # https://github.com/NixOS/nixpkgs/blob/db24d86dd8a4769c50d6b7295e81aa280cd93f35/pkgs/applications/editors/neovim/wrapper.nix#L13
-      extraName = "";
-      withPython2 = false;
-      vimAlias = true; # defaults to false
-      viAlias = false;
-      wrapRc = true;
-      neovimRcContent = "";
-    });
+        plugins = with pkgs.vimPlugins; [
+          # Telescope
+          telescope-fzf-native-nvim
+          nvim-web-devicons
+          plenary-nvim
+          telescope-nvim
+          (nvim-treesitter.withPlugins (p: with p; [
+            nix
+            go
+            rust
+            bash
+            fish
+          ]))
+        ];
 
-    luaInit = pkgs.writeText "init.lua" ''
+        # https://github.com/NixOS/nixpkgs/blob/db24d86dd8a4769c50d6b7295e81aa280cd93f35/pkgs/applications/editors/neovim/wrapper.nix#L13
+        extraName = "";
+        withPython2 = false;
+        vimAlias = true; # defaults to false
+        viAlias = false;
+        wrapRc = false;
+        neovimRcContent = "";
+      });
+
+      luaInit = pkgs.writeText "init.lua" ''
         vim.api.nvim_command('set number')
         vim.api.nvim_command('set relativenumber')
-        print("Hello")
-    '';
 
-    marcusNeovim = { stdenv, makeBinaryWrapper }: stdenv.mkDerivation {
+        -- Telescope --
+
+        -- https://github.com/nvim-telescope/telescope.nvim#usage
+        local telescopeBuiltin = require('telescope.builtin')
+
+        vim.keymap.set('n', '<leader>ff', telescopeBuiltin.find_files, {})
+        vim.keymap.set('n', '<leader>fg', telescopeBuiltin.live_grep, {})
+        vim.keymap.set('n', '<leader>fb', telescopeBuiltin.buffers, {})
+        vim.keymap.set('n', '<leader>fh', telescopeBuiltin.help_tags, {})
+
+        -- Treesitter --
+
+        require'nvim-treesitter.configs'.setup {
+          highlight = {
+            enable = true,
+          },
+        }
+
+      '';
+    in stdenv.mkDerivation {
       pname = "marcus-neovim";
       version = "unstable";
       src = ./.;
@@ -40,12 +72,20 @@
       nativeBuildInputs = [ makeBinaryWrapper ];
 
       installPhase = ''
-        mkdir $out;
-        makeWrapper ${neovim}/bin/nvim $out/bin/vim --add-flags "-u ${luaInit}"
+        mkdir -p $out/bin
+        mkdir -p $out/config
+        cp ${luaInit} $out/config/init.lua
+        makeWrapper ${neovim}/bin/nvim $out/bin/vim --add-flags "-u $out/config/init.lua"
       '';
     };
   in rec {
     packages.marcus-neovim = pkgs.callPackage marcusNeovim {};
     packages.default = packages.marcus-neovim;
+
+    apps.marcus-neovim = {
+      type = "app";
+      program = "${self.packages.${system}.marcus-neovim}/bin/vim";
+    };
+    apps.default = apps.marcus-neovim;
   });
 }
